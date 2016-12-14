59dd78c6c5774f2a104ecd2bccc0a0cc1b6b3165
Zhangbeiyi <zhangbeiyi@zhangbeiyidemacbook-air.local>
2016-12-15 07:36:36 +0800

    Initial Commit

---
+++ apkInstallHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  apkInstallHelper
+//
+//  Created by Zhangbeiyi on 2016/12/15.
+//  Copyright © 2016年 Zhangbeiyi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ apkInstallHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  apkInstallHelper
+//
+//  Created by Zhangbeiyi on 2016/12/15.
+//  Copyright © 2016年 Zhangbeiyi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
